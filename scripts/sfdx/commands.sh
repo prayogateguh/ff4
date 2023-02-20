4151e3bd28f9c3741e5b16186189dfa5e479dc16
Prayoga Teguh <teguh.mow@gmail.com>
2023-02-20 14:41:33 +0700

    Chapter 2

---
+++ scripts/sfdx/commands.sh
@@ -0,0 +1,3 @@
+sfdx force:user:permset:assign -n FormulaForceRaceManagementUpdateDNFStatus
+sfdx force:user:permset:assign -n FormulaForceRaceManagement
+sfdx force:user:permset:assign -n FormulaForceRaceAnalytics
